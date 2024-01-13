f30e8cd108f4a8a04e318a06aee1a78696e1aaaf
fduranpa <francolionel.d@gmail.com>
2024-01-13 16:19:53 -0300

    se cambia puerto 3001 por defecto

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,6 +2,6 @@
 export const EnvConfiguration = () => ({
     environment: process.env.NODE_ENV || 'dev',
     mongoDb: process.env.MONGODB,
-    port: process.env.PORT || 3002,
+    port: process.env.PORT || 3001,
     defaultLimit: process.env.DEFAULT_LIMIT || 7,
 })
